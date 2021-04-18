693386451f03c8189934280dcff79e80018ae438
Edmund Hee <edmund.hee05@gmail.com>
2021-04-18 17:07:18 +0800

    Day 2 Commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,6 +12,9 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+    script: [
+      {crossorigin: 'anonymous', type: 'text/javascript', src: "https://kit.fontawesome.com/48d20e9c42.js"}
     ]
   },
 
---
+++ pages/product/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>This is product index.vue</div>
+</template>
+
+<script>
+export default {
+    layout: "layout_a"
+}
+</script>
+
+<style>
+
+</style>
